8c2fa4222402660b72ed09c74cbc304d8a3d7fb9
SarvajithKarun <sarvajithkarun@axablock.com>
2025-06-07 19:54:03 -0500

    first commit 6

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -90,7 +90,7 @@ const ContactPage = () => {
                 } else {
                   setResult(data.message || "Something went wrong. Please try again.");
                 }
-              } catch (_) {
+              } catch {
                 setResult("Network error. Please try again.");
               } finally {
                 setIsSubmitting(false);
